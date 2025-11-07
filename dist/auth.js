a0a2b32577c67b3e2681bcebfe29b68e1f462200
SunnyGitGud <wecandothissunny@gmail.com>
2025-11-07 23:17:03 +0530

    auth test cases

--- dist/auth.js
+++ dist/auth.js
@@ -1,4 +1,5 @@
 import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
 export async function hashPassword(password) {
     const hash = await argon2.hash(password);
     return hash;
@@ -6,4 +7,20 @@ export async function hashPassword(password) {
 export async function checkPasswordHash(password, hash) {
     const isValid = await argon2.verify(password, hash);
     return isValid;
+}
+export function makeJWT(userID, expiresIn, secret) {
+    const iat = Math.floor(Date.now() / 1000);
+    const exp = Number(expiresIn) + iat;
+    const payload = {
+        iss: "chirpy",
+        sub: userID,
+        iat,
+        exp
+    };
+    const token = jwt.sign(payload, secret);
+    return token;
+}
+export function validateJWT(tokenString, secret) {
+    var decoded = jwt.verify(tokenString, secret);
+    return decoded.sub;
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import * as argon2 from "argon2";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { unAuthorized401 } from "./error";
 
 export async function hashPassword(password: string) {
   const hash = await argon2.hash(password)
@@ -6,8 +8,30 @@ export async function hashPassword(password: string) {
 }
 
 export async function checkPasswordHash(password: string, hash: string) {
-  const isValid = await argon2.verify(password, hash)
-  return isValid
+  return argon2.verify(password, hash).then(result => result).catch(() => false)
 }
 
+type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">
+
+export function makeJWT(userID: string, expiresIn: string | number, secret: string): string {
+  const iat = Math.floor(Date.now() / 1000)
+  const exp = Number(expiresIn) + iat
+  const payload: payload = {
+    iss: "chirpy",
+    sub: userID,
+    iat,
+    exp
+  }
+  const token = jwt.sign(payload, secret)
+  return token;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+  try {
+    var decoded = jwt.verify(tokenString, secret) as payload
+    return decoded.sub as string
+  } catch {
+    throw new unAuthorized401("UnAuthorized token")
+  }
+}
 
